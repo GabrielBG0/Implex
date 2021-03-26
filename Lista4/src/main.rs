b78f57999c7b782bcaa0a70a7e47d3542419612b
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-03-25 22:29:18 -0300

    snake case

--- Lista4/src/main.rs
+++ Lista4/src/main.rs
@@ -1,4 +1,4 @@
-fn unpackVector(vetor: Vec<char>) -> Vec<char>{
+fn unpack_vector(vetor: Vec<char>) -> Vec<char>{
     let mut pilha: Vec<char> = vec![];
     for c in vetor.iter(){
         if *c == '#'{
@@ -17,10 +17,10 @@ fn unpackVector(vetor: Vec<char>) -> Vec<char>{
 }
 
 fn main() {
-    let vetorTeste1 = vec!['A','B','R','A','C','O','B','R','#','#','#','A','D','A','B','R','A'];
-    let vetorTeste2 = vec!['A','B','R','A','C','A','@','A','L','A','K','A','S','#','Z','A','M'];
-    let resultado1 = unpackVector(vetorTeste1);
-    let resultado2 = unpackVector(vetorTeste2);
-    println!(" teste 1: {:?}", resultado1);
-    println!(" teste 2: {:?}", resultado2);
+    let vetor_teste_1 = vec!['A','B','R','A','C','O','B','R','#','#','#','A','D','A','B','R','A'];
+    let vetor_teste_2 = vec!['A','B','R','A','C','A','@','A','L','A','K','A','S','#','Z','A','M'];
+    let resultado_1 = unpack_vector(vetor_teste_1);
+    let resultado_2 = unpack_vector(vetor_teste_2);
+    println!(" teste 1: {:?}", resultado_1);
+    println!(" teste 2: {:?}", resultado_2);
 }
