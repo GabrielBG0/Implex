2827dff9995964a2a9059a1e9f49cbfe5bd55efb
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-04-11 17:18:48 -0300

    ex prog 2 100%

--- lista_8/main.py
+++ lista_8/main.py
@@ -11,6 +11,16 @@ class Grafo:
         self.id = id
 
 
+def escreveDocumento(y, n, m, matris, id):
+    path = "resultado/" + id + ".txt"
+    headder = str(n) + " " + str(m)
+    if y == 3:
+        np.savetxt(fname=path, X=matris, header=headder,
+                   fmt="%01i", delimiter=" -> ")
+    else:
+        np.savetxt(fname=path, X=matris, header=headder, fmt="%01i")
+
+
 if __name__ == '__main__':
     paths = []
 
@@ -32,7 +42,9 @@ if __name__ == '__main__':
             content.append(string.replace("\n", ''))
         id = path.rstrip(".tx")
         id = id[7:]
-        grafos.append(Grafo(int(x), int(y), int(n), int(m), content, int(id)))
+        grafos.append(Grafo(int(x), int(y), int(n), int(m), content, id))
+
+    print("trasncrevendo documentos...")
 
     for grafo in grafos:
         if grafo.x == 1:
@@ -41,14 +53,14 @@ if __name__ == '__main__':
                 for i in grafo.content:
                     (a, b) = i.split(' ')
                     matris[int(a) - 1][int(b) - 1] = 1
-                # escrever no documento
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
             elif grafo.y == 2:
                 matris = np.zeros((grafo.n, grafo.m))
                 for index, par in enumerate(grafo.content):
                     (a, b) = par.split(' ')
                     matris[int(a) - 1][index] = 1
                     matris[int(b) - 1][index] = 1
-                # escrever no documento
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
             elif grafo.y == 3:
                 matris = np.zeros((grafo.n, grafo.m))
                 matris[0][0] = 1
@@ -70,14 +82,14 @@ if __name__ == '__main__':
                             if matris[int(b) - 1][index] == 0:
                                 matris[int(b) - 1][index] = int(a)
                                 break
-                # escrever no documento
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
         elif grafo.x == 2:
             if grafo.y == 1:
                 matris = np.zeros((grafo.n, grafo.n))
                 for i in range(0, grafo.n):
                     for j in range(0, grafo.n):
-                        matris[i][j] = grafo.content[i][j]
-                # escrever no documento
+                        matris[i][j] = int(grafo.content[i][j])
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
             elif grafo.y == 2:
                 matris = np.zeros((grafo.n, grafo.m))
                 pares = []
@@ -90,7 +102,7 @@ if __name__ == '__main__':
                 for i in range(grafo.m):
                     matris[pares[i][0]][i] = 1
                     matris[pares[i][1]][i] = 1
-                # escrever no documento
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
             elif grafo.y == 3:
                 matris = np.zeros((grafo.n, grafo.m))
                 matris[0][0] = 1
@@ -108,13 +120,48 @@ if __name__ == '__main__':
                             if matris[par[0]][index] == 0:
                                 matris[par[0]][index] = par[1] + 1
                                 break
-                # escrever no documento
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
         elif grafo.x == 3:
             if grafo.y == 1:
-                print(y)
+                matris = np.zeros((grafo.n, grafo.n))
+                for i in range(grafo.m):
+                    par = []
+                    for j in range(grafo.n):
+                        if grafo.content[j][i] == "2":
+                            matris[j][j] = 1
+                            break
+                        elif grafo.content[j][i] == "1":
+                            par.append(j)
+                    matris[par[0]][par[1]] = 1
+                    matris[par[1]][par[0]] = 1
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
             elif grafo.y == 2:
-                print(y)
+                matris = np.zeros((grafo.n, grafo.m))
+                for i in range(grafo.n):
+                    for j in range(grafo.m):
+                        matris[i][j] = int(grafo.content[i][j])
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
             elif grafo.y == 3:
-                print(y)
-
+                matris = np.zeros((grafo.n, grafo.m))
+                matris[0][0] = 1
+                pares = []
+                for i in range(grafo.m):
+                    par = []
+                    for j in range(grafo.n):
+                        if grafo.content[j][i] == "2":
+                            matris[j][j] = 1
+                            break
+                        elif grafo.content[j][i] == "1":
+                            par.append(j)
+                    pares.append(par)
+                for par in pares:
+                    if matris[par[0]][0] != par[0] + 1:
+                        matris[par[0]][0] = par[0] + 1
+                        matris[par[0]][1] = par[1] + 1
+                    else:
+                        for index in range(1, grafo.m):
+                            if matris[par[0]][index] == 0:
+                                matris[par[0]][index] = par[1] + 1
+                                break
+                escreveDocumento(grafo.y, grafo.n, grafo.m, matris, grafo.id)
     print("programa concluido")
